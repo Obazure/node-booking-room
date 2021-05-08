89b44646afc04aaf38c0928fcc2c7ba2da42593a
Yeldar Kossynbay <yeldar.kossynbay@sport1.de>
2021-05-08 15:19:25 +0200

    adjust isValidId validation

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,7 +1,7 @@
 /**
  * @return boolean -> true - valid, false - not valid
  */
-export const isValidId = (id: any): boolean => !isNaN(id)
+export const isValidId = (id: any): boolean => id && !isNaN(id) && id !== 0
 
 /**
  * @return boolean -> true - valid, false - not valid
