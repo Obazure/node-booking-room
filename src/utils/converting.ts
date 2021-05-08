50969b387ca24707db4449b2f1c234adfcf4b453
Yeldar Kossynbay <yeldar.kossynbay@sport1.de>
2021-05-08 23:26:33 +0200

    fix converting

--- src/utils/converting.ts
+++ src/utils/converting.ts
@@ -1,6 +1,6 @@
 export const convertToDate = (value: string | number): Date => {
     const date = new Date(value)
-    date.setMinutes(0)
+    date.setMilliseconds(0)
     date.setSeconds(0)
     return date
 }
