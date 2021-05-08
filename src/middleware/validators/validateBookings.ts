328d601cbbf8bf8a0b349c4b1a6d93eb5b095a71
Yeldar Kossynbay <yeldar.kossynbay@sport1.de>
2021-05-08 23:27:34 +0200

    id should be optional

--- src/middleware/validators/validateBookings.ts
+++ src/middleware/validators/validateBookings.ts
@@ -1,16 +1,12 @@
 import { NextFunction, Response } from 'express'
 import { BookingsSaveRequest } from '../../@types/requests'
-import { isValidDate, isValidId, isValidString } from '../../utils/validation'
+import { isValidDate, isValidString } from '../../utils/validation'
 
 const validateBookings = (req: BookingsSaveRequest, res: Response, next: NextFunction): void => {
     const bookings = req.body
     if (Array.isArray(bookings)) {
         const bookingsAreValid = bookings.every(booking => {
             return Object.entries(booking).every(([field, value]) => {
-                if (field === 'id' && !isValidId(value)) {
-                    return false
-                }
-
                 if (['first_name', 'last_name'].includes(field) && !isValidString(value)) {
                     return false
                 }
