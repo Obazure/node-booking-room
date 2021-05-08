2efed1e7b9afaa9643426645b9e6c231017501a0
Yeldar Kossynbay <yeldar.kossynbay@sport1.de>
2021-05-08 15:19:48 +0200

    add booking removal endpoint

--- src/repositories/BookingRepository.ts
+++ src/repositories/BookingRepository.ts
@@ -6,8 +6,13 @@ const BookingRepository = {
         return false
     },
     async get(periods: FilterPeriod): Promise<Booking[]> {
+        // TODO logic
         return []
-    }
+    },
+    async remove(bookingId: number): Promise<boolean> {
+        // TODO logic
+        return false
+    },
 }
 
 export default BookingRepository
--- src/routes/apiBookingRouter.ts
+++ src/routes/apiBookingRouter.ts
@@ -5,6 +5,7 @@ import validateBookings from '@/middleware/validators/validateBookings'
 import convertBookingsTime from '@/middleware/convertors/convertBookingsTime'
 import validatePeriodInParams from '@/middleware/validators/validatePeriodInParams'
 import convertPeriodInParams from '@/middleware/convertors/convertPeriodInParams'
+import { isValidId } from '@/utils/validation'
 
 const apiBookingRouter = express.Router()
 
@@ -33,5 +34,16 @@ apiBookingRouter.get(
         res.status(200).json(bookings)
     }
 )
+
+apiBookingRouter.delete('/:id', async (req, res) => {
+    const bookingId = Number(req.params.id)
+    if (bookingId && isValidId(bookingId)) {
+        const removalStatus = await BookingRepository.remove(bookingId)
+        if (removalStatus) {
+            res.status(200).send('Successful operation')
+        }
+    }
+    res.status(405).send('Invalid input')
+})
 
 export default apiBookingRouter
