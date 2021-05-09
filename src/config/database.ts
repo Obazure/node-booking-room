b1c299a72e12e2e308bddff7b880c8890b8948c9
Yeldar Kossynbay <yeldar.kossynbay@sport1.de>
2021-05-09 07:52:41 +0200

    change knex to sequilize

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,7 +1,21 @@
-import Knex from 'knex'
-import connectionSettings from '../config/knexfile'
+import { Options, Sequelize } from 'sequelize'
 
-const database = Knex(connectionSettings)
-export type Database = typeof database
+const isDevelopment = process.env.NODE_ENV === 'development'
 
-export default database
+const MYSQL_DATABASE = process.env.MYSQL_DATABASE || 'test'
+const MYSQL_USER = process.env.MYSQL_USER || 'root'
+const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD || ''
+const DB_HOST = process.env.DB_HOST || '127.0.0.1'
+const DB_PORT = process.env.DB_PORT || 3306
+
+const dbOptions: Options = {
+    host: DB_HOST as string,
+    port: DB_PORT as number,
+    dialect: 'mysql',
+}
+
+if (!isDevelopment) {
+    dbOptions.logging = false
+}
+
+export const sequelize = new Sequelize(MYSQL_DATABASE, MYSQL_USER, MYSQL_PASSWORD, dbOptions)
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 import express from 'express'
 import apiBookingRouter from './routes/apiBookingRouter'
+import { sequelize } from './config/database'
+import { log } from './utils/logger'
 
 const app = express()
 app.use(express.json())
 
 app.use('/booking', apiBookingRouter)
 
-const PORT = process.env.NODE_ENV || 4000
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}.`)
-})
+async function start(): Promise<void> {
+    await sequelize.sync() // {alter: true}
+    const PORT = process.env.NODE_ENV || 4000
+    app.listen(PORT, () => {
+        log.info(`Server started on port ${PORT}.`)
+    })
+}
 
-export default app
+start().then(() => {
+    log.info('Server run')
+})
